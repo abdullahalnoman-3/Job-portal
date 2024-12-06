16be2dcf1905206c4b2223fdb72f7469ed1f2258
AbdulaalNoman <167016116+AbdulaalNoman@users.noreply.github.com>
2024-12-06 23:44:15 +0600

    nm1.2

--- resources/views/pages/admin/admin_db_input.blade.php
+++ resources/views/pages/admin/admin_db_input.blade.php
@@ -11,27 +11,11 @@
       Admin Panel
     </div>
     <div style="margin-top: 20px;">
-      <a href="#dashboard" style="display: block; text-decoration: none; color: white; padding: 15px 20px; font-weight: 600; background-color: transparent;">
-        <i class="bi bi-speedometer2" style="margin-right: 10px;"></i> Dashboard
-      </a>
-      <a href="#jobs" style="display: block; text-decoration: none; color: white; padding: 15px 20px; font-weight: 600; background-color: transparent;">
-        <i class="bi bi-briefcase" style="margin-right: 10px;"></i> Manage Jobs
-      </a>
-      <a href="#applications" style="display: block; text-decoration: none; color: white; padding: 15px 20px; font-weight: 600; background-color: transparent;">
-        <i class="bi bi-file-earmark-text" style="margin-right: 10px;"></i> Applications
-      </a>
-      <a href="#users" style="display: block; text-decoration: none; color: white; padding: 15px 20px; font-weight: 600; background-color: transparent;">
-        <i class="bi bi-people" style="margin-right: 10px;"></i> Users
-      </a>
+      
       <a href="/admin_db_input" style="display: block; text-decoration: none; color: white; padding: 15px 20px; font-weight: 600; background-color: transparent;">
         <i class="bi bi-people" style="margin-right: 10px;"></i> Dropdown input
       </a>
-      <a href="#reports" style="display: block; text-decoration: none; color: white; padding: 15px 20px; font-weight: 600; background-color: transparent;">
-        <i class="bi bi-bar-chart" style="margin-right: 10px;"></i> Reports
-      </a>
-      <a href="#settings" style="display: block; text-decoration: none; color: white; padding: 15px 20px; font-weight: 600; background-color: transparent;">
-        <i class="bi bi-gear" style="margin-right: 10px;"></i> Settings
-      </a>
+   
     </div>
   </div>
 

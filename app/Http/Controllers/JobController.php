5315c85fa6fec0fd6947d9fd40e22fc82f35ff88
Karamul Ambia Mahdi <76122629+Karamul-Ambia-Mahdi@users.noreply.github.com>
2024-12-10 02:09:22 +0600

    1.04

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
+use App\Models\User;
 use App\Models\citie;
 use App\Models\countrie;
-use App\Models\experience_level;
-use App\Models\Job;
 use App\Models\Job_Role;
-use App\Models\User;
+use App\Models\WorkMode;
+use App\Models\Job_types;
 use Illuminate\Http\Request;
+use App\Models\Job_functions;
+use App\Models\experience_level;
 
 class JobController extends Controller
 {
@@ -19,7 +22,12 @@ class JobController extends Controller
         $jobroles = Job_Role::orderBy('id', 'desc')->get();
         $cityname = citie::orderBy('id', 'desc')->get();
         $countrye = countrie::orderBy('id', 'desc')->get(); 
-        return view("pages.employer.job_post", compact('countrye','cityname','jobroles','jobLevels'));
+
+        $jobTypes = job_types::orderBy('id', 'desc')->get();
+        $jobFunctions = job_functions::orderBy('id', 'desc')->get();
+        $jobWorkModes = WorkMode::orderBy('id', 'desc')->get();
+
+        return view("pages.employer.job_post", compact('countrye','cityname','jobroles','jobLevels', 'jobTypes', 'jobFunctions', 'jobWorkModes'));
     }    
 
     public function job_post_store(Request $request)
@@ -29,16 +37,21 @@ class JobController extends Controller
         // $user = User::where('email', '=', $request->header('email'))->first();
         // $userEmail = $request->header('email');
         // $userId = $request->header('id');
+
         // Directly insert into the database without validation
         Job::create([
 
             'job_title' => $request->input('job_title'),
+            'company_name' => $request->input('companyName'),
+            'company_website' => $request->input('companyWebsite'),
+            'job_type_id' => $request->input('jobType'),
             'job_role_id' => $request->input('job_role_id'),
             'job_tags' => $request->input('tags'),
+            'job_function_id' => $request->input('jobFunction'),
             'min_salary' => $request->input('minSalary'),
             'max_salary' => $request->input('maxSalary'),
-            'remark' => $request->input('salaryType'),
             'vacancies' => $request->input('vacancies'),
+            'work_mode_id' => $request->input('jobWorkMode'),
             'experience_level_id' => $request->input('jobLevel'),
             'country_id' => $request->input('country_id'),
             'city_id' => $request->input('city_id'),
--- resources/views/pages/employer/job_post.blade.php
+++ resources/views/pages/employer/job_post.blade.php
@@ -1,28 +1,29 @@
-@extends('layout.app',['title' => 'Employers'])
+@extends('layout.app', ['title' => 'Employers'])
 
 @section('content')
-
     <div class="container form-container my-5">
         <div class="form-title">Post a job</div>
         <div class="form-subtitle">Find the best talent for your company</div>
         <form id="postJobForm" method="post" action="{{ route('job_post_store') }}">
-        @csrf
+            @csrf
             <div class="mb-3">
                 <label for="jobTitle" class="form-label">Job Title</label>
-                <input type="text" class="form-control" name="job_title" id="jobTitle" placeholder="Add job title, role vacancies etc">
+                <input type="text" class="form-control" name="job_title" id="jobTitle"
+                    placeholder="Add job title, role vacancies etc">
             </div>
             <div class="row">
                 <div class="col-md-6 mb-3">
                     <label for="tags" class="form-label">Tags</label>
-                    <input type="text" class="form-control" name="tags" id="tags" placeholder="Job keyword, tags etc">
+                    <input type="text" class="form-control" name="tags" id="tags"
+                        placeholder="Job keyword, tags etc">
                 </div>
                 <div class="col-md-6 mb-3">
                     <label for="jobRole" class="form-label">Job Role</label>
                     <select class="form-select" id="jobRole" name="job_role_id">
-                    <option value="">Select</option>
-                    @foreach($jobroles as $jobrole)
-                        <option value="{{ $jobrole->id }}">{{ $jobrole->job_role_name }}</option>
-                    @endforeach
+                        <option value="">Select</option>
+                        @foreach ($jobroles as $jobrole)
+                            <option value="{{ $jobrole->id }}">{{ $jobrole->job_role_name }}</option>
+                        @endforeach
                     </select>
                 </div>
             </div>
@@ -31,7 +32,8 @@
                     <label for="minSalary" class="form-label">Min Salary</label>
                     <div class="container">
                         <div class="d-flex align-items-center justify-content-between">
-                            <input type="text" name="minSalary" class="form-control" id="minSalary" placeholder="Minimum Salary...">
+                            <input type="text" name="minSalary" class="form-control" id="minSalary"
+                                placeholder="Minimum Salary...">
                             <span class="input-group-text">BDT</span>
                         </div>
                     </div>
@@ -46,15 +48,37 @@
                     </div>
                 </div>
                 <div class="col-md-6 mb-3">
-                    <label for="salaryType" class="form-label">Salary Type</label>
-                    <select class="form-select" id="salaryType" name="salaryType">
-                        <option >Select</option>
-                        <option >1</option>
-                        <option >2</option>
-                        <option >3</option>
+                    <label for="jobType" class="form-label">Job Type</label>
+                    <select class="form-select" id="jobType" name="jobType">
+                        <option value="">Select</option>
+                        @foreach ($jobTypes as $jobType)
+                            <option value="{{ $jobType->id }}">{{ $jobType->job_type_name }}</option>
+                        @endforeach
                     </select>
                 </div>
             </div>
+
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <label for="jobFunction" class="form-label">Job Function</label>
+                    <select class="form-select" id="jobFunction" name="jobFunction">
+                        <option value="">Select</option>
+                        @foreach ($jobFunctions as $jobFunction)
+                            <option value="{{ $jobFunction->id }}">{{ $jobFunction->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label for="jobWorkMode" class="form-label">Job Work Mode</label>
+                    <select class="form-select" id="jobWorkMode" name="jobWorkMode">
+                        <option value="">Select</option>
+                        @foreach ($jobWorkModes as $jobWorkMode)
+                            <option value="{{ $jobWorkMode->id }}">{{ $jobWorkMode->work_mode_name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+
             <div class="row">
                 <div class="col-md-6 mb-3">
                     <label for="vacancies" class="form-label">Vacancies</label>
@@ -63,30 +87,44 @@
                 <div class="col-md-6 mb-3">
                     <label for="jobLevel" class="form-label">Job Level</label>
                     <select class="form-select" id="jobLevel" name="jobLevel">
-                    <option value="">Select</option>
-                    @foreach($jobLevels as $jobLevel)
-                        <option value="{{ $jobLevel->id }}">{{ $jobLevel->experience_name }}</option>
-                    @endforeach
+                        <option value="">Select</option>
+                        @foreach ($jobLevels as $jobLevel)
+                            <option value="{{ $jobLevel->id }}">{{ $jobLevel->experience_name }}</option>
+                        @endforeach
                     </select>
                 </div>
             </div>
+
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <label for="companyName" class="form-label">Company Name</label>
+                    <input type="text" class="form-control" name="companyName" id="companyName"
+                        placeholder="Google, Microsoft, etc">
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label for="companyWebsite" class="form-label">Company Website</label>
+                    <input type="text" class="form-control" name="companyWebsite" id="companyWebsite"
+                        placeholder="www.google.com, etc">
+                </div>
+            </div>
+
             <div class="row">
                 <div class="col-md-6 mb-3">
                     <label for="country" class="form-label">Country</label>
                     <select class="form-select" id="country" name="country_id">
-                    <option value="">Select</option>
-                    @foreach($countrye as $country)
-                        <option value="{{ $country->id }}">{{ $country->country_name }}</option>
-                    @endforeach
+                        <option value="">Select</option>
+                        @foreach ($countrye as $country)
+                            <option value="{{ $country->id }}">{{ $country->country_name }}</option>
+                        @endforeach
                     </select>
                 </div>
                 <div class="col-md-6 mb-3">
                     <label for="city" class="form-label">City</label>
                     <select class="form-select" id="city" name="city_id">
-                    <option value="">Select</option>
-                    @foreach($cityname as $city)
-                        <option value="{{ $city->id }}">{{ $city->city_name }}</option>
-                    @endforeach
+                        <option value="">Select</option>
+                        @foreach ($cityname as $city)
+                            <option value="{{ $city->id }}">{{ $city->city_name }}</option>
+                        @endforeach
                     </select>
                 </div>
             </div>
@@ -115,5 +153,4 @@
             <button type="submit" class="btn btn-primary py-3 px-5">Post Job</button>
         </form>
     </div>
-
 @endSection
